edfa5a0deee7472fd4a5e963eb7e1b84b3bd7b73
edvxndxvi <edvandavi1@hotmail.com>
2026-07-22 11:14:58 -0300

    add Sidebar

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,15 +1,13 @@
-import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
-
-const url = process.env.DATABASE_URL!;
+import 'dotenv/config';
+import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  engine: "classic",
-  datasource: {
-    url,
-  },
+    schema: 'prisma/schema.prisma',
+    migrations: {
+        path: 'prisma/migrations',
+    },
+    engine: 'classic',
+    datasource: {
+        url: env('DATABASE_URL'),
+    },
 });
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/styles/globals.css';
+import { Sidebar } from '@/components/sidebar';
 
 const inter = Inter({
     variable: '--font-sans',
@@ -25,6 +26,7 @@ export default function RootLayout({
         >
             <body className="min-h-full flex flex-col">
                 <section className="flex h-screen">
+                    <Sidebar />
                     <main className="relative flex-1 overflow-auto min-w-0">
                         <div className="p-4 sm:p-6 md:p-8 max-w-full md:max-w-3xl mx-auto h-full">
                             {children}
